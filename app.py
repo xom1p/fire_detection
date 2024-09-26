e4a174384400f2a49664954b9e0dcbc69701a9d0
gugunmo <131753649+gugunmo@users.noreply.github.com>
2024-09-26 11:14:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 import io
 import PIL
 from PIL import Image
+from camera_input_live import camera_input_live
 
 def play_video(video_source):
     camera = cv2.VideoCapture(video_source)
@@ -63,5 +64,12 @@ if source_radio == "VIDEO":
             pass
 
 
+def play_live_camera():
+    image = camera_input_live()
+    uploaded_image = PIL.Image.open(image)
+    uploaded_image_cv = cv2.cvtColor(np.array(uploaded_image), cv2.COLOR_RGB2BGR)
+    visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold)
+    st.image(visualized_image, channels = "BGR")
+
 if source_radio == "WEBCAM":
-    play_video(0)
+    play_live_camera()
